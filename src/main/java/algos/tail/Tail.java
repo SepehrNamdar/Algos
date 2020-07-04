9b69d5bac7ea71e17ebce7a973a6dd8ed5c3fcad
Sepehr NAMDAR <sepehr_gsfc@yahoo.com>
2020-07-04 13:12:06 +0200

    end

--- src/main/java/algos/tail/Tail.java
+++ src/main/java/algos/tail/Tail.java
@@ -4,25 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Tail {
-    private int lineNumbers;
+    private final int lineNumbers;
 
     public Tail(int lineNumbers) {
         this.lineNumbers = lineNumbers;
     }
 
-    public List<String> get(List<String> givenLines) {
+    public List<String> get(final List<String> givenLines) {
         List<String> tail = new ArrayList<>();
-        if (lineNumbers == 0) {
-            return new ArrayList<>();
-        } else if (lineNumbers == 1) {
-            tail.add(givenLines.get(2));
-        } else if (lineNumbers == 2) {
-            tail.add(givenLines.get(1));
-            tail.add(givenLines.get(2));
-        } else if (lineNumbers == 3) {
-            tail.add(givenLines.get(0));
-            tail.add(givenLines.get(1));
-            tail.add(givenLines.get(2));
+        int givenLinesLength = givenLines.size();
+        int remainingLines = givenLinesLength - lineNumbers;
+        for (int index = 0; index < givenLinesLength - remainingLines; index++) {
+            tail.add(givenLines.get(remainingLines + index));
         }
         return tail;
     }
