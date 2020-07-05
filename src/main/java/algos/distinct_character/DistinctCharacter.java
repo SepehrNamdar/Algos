29b5e71dc20aa6f23eca12e004c4e422b83dfd48
Sepehr NAMDAR <sepehr_gsfc@yahoo.com>
2020-07-05 02:21:15 +0200

    get_a_non_redundant_string

---
+++ src/main/java/algos/distinct_character/DistinctCharacter.java
@@ -0,0 +1,13 @@
+package algos.distinct_character;
+
+public class DistinctCharacter {
+    private final String aString;
+
+    public DistinctCharacter(String aString) {
+        this.aString = aString;
+    }
+
+    public String getCharacters() {
+        return aString;
+    }
+}
---
+++ src/test/java/algos/DistinctCharacterShould.java
@@ -0,0 +1,16 @@
+package algos;
+
+import algos.distinct_character.DistinctCharacter;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DistinctCharacterShould {
+
+    @Test
+    void get_a_non_redundant_string() {
+        assertThat(new DistinctCharacter("").getCharacters()).isEqualTo("");
+        assertThat(new DistinctCharacter("a").getCharacters()).isEqualTo("a");
+    }
+}
