1c44553b980412d61585e845c1992784f9a653b8
Sepehr NAMDAR <sepehr_gsfc@yahoo.com>
2020-07-05 22:46:26 +0200

    end

--- src/main/java/algos/sorted_matrix/SortedMatrix.java
+++ src/main/java/algos/sorted_matrix/SortedMatrix.java
@@ -1,13 +1,24 @@
 package algos.sorted_matrix;
 
-public class SortedMatrix {
-    private final int[][] matrix;
+import java.util.ArrayList;
+import java.util.List;
 
-    public SortedMatrix(int[][] matrix) {
+import static java.util.Arrays.asList;
+import static java.util.Comparator.naturalOrder;
+
+public class SortedMatrix {
+    private final Integer[][] matrix;
+
+    public SortedMatrix(Integer[][] matrix) {
         this.matrix = matrix;
     }
 
-    public int[][] sort() {
-        return new int[0][0];
+    public List<Integer> sort() {
+        List<Integer> list = new ArrayList<>();
+        for (Integer[] array : matrix) {
+            list.addAll(asList(array));
+        }
+        list.sort(naturalOrder());
+        return list;
     }
 }
--- src/test/java/algos/sorted_matrix/SortedMatrixShould.java
+++ src/test/java/algos/sorted_matrix/SortedMatrixShould.java
@@ -2,16 +2,63 @@ package algos.sorted_matrix;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SortedMatrixShould {
 
     @Test
     void sort_an_empty_matrix() {
-        int[][] matrix = new int[0][0];
+        Integer[][] matrix = new Integer[0][0];
 
         SortedMatrix sm = new SortedMatrix(matrix);
 
         assertThat(sm.sort()).isEmpty();
+    }
+
+    @Test
+    void sort_a_one_element_matrix() {
+        Integer[][] matrix = new Integer[1][1];
+        matrix[0][0] = 2;
+
+        SortedMatrix sm = new SortedMatrix(matrix);
+
+        List<Integer> result = new ArrayList<>();
+        result.add(2);
+        assertThat(sm.sort()).isEqualTo(result);
+    }
+
+    @Test
+    void sort_a_1_2_dimension_matrix() {
+        Integer[][] matrix = new Integer[1][2];
+        matrix[0][0] = 1;
+        matrix[0][1] = 2;
+
+        SortedMatrix sm = new SortedMatrix(matrix);
+
+        List<Integer> result = new ArrayList<>();
+        result.add(1);
+        result.add(2);
+        assertThat(sm.sort()).isEqualTo(result);
+    }
+
+    @Test
+    void sort_a_2_2_dimension_matrix() {
+        Integer[][] matrix = new Integer[2][2];
+        matrix[0][0] = 1;
+        matrix[0][1] = 3;
+        matrix[1][0] = 2;
+        matrix[1][1] = 4;
+
+        SortedMatrix sm = new SortedMatrix(matrix);
+
+        List<Integer> result = new ArrayList<>();
+        result.add(1);
+        result.add(2);
+        result.add(3);
+        result.add(4);
+        assertThat(sm.sort()).isEqualTo(result);
     }
 }
