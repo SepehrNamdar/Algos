863bc0f464e9259817154d1d2eb0c3f824bc23a8
Sepehr NAMDAR <sepehr_gsfc@yahoo.com>
2020-07-05 02:42:49 +0200

    one of the lists is empty

--- src/test/java/algos/CommonShould.java
+++ src/test/java/algos/CommonShould.java
@@ -5,7 +5,9 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CommonShould {
@@ -13,6 +15,10 @@ public class CommonShould {
     @Test
     void find_common_characters_in_2_sorted_lists() {
         assertThat(new Commun(new ArrayList(), new ArrayList()).getCommonCharacters())
+                .isEqualTo(new ArrayList<>());
+        assertThat(new Commun(new ArrayList(), asList('a', 'b')).getCommonCharacters())
+                .isEqualTo(new ArrayList<>());
+        assertThat(new Commun(asList('a', 'b'), new ArrayList()).getCommonCharacters())
                 .isEqualTo(new ArrayList<>());
     }
 }
